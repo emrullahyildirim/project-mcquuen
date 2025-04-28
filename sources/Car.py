edebbe3af9ebca8c58c22b02d5fa6bfcd96201ea
humeyra saritop <humeyrasaritop@gmail.com>
2025-04-28 18:19:00 +0300

    araba

--- sources/Car.py
+++ sources/Car.py
@@ -1,3 +1,22 @@
+import pygame
+
 class Car:
-	pass
-	
+    def __init__(self):
+        # Arabamızın başlangıç pozisyonu ve boyutu
+        self.rect = pygame.Rect(100, 100, 50, 30)  # (x, y, genişlik, yükseklik)
+        self.color = (255, 0, 0)  # Şimşek McQueen kırmızısı
+        self.speed = 5  # Arabamızın hareket hızı
+
+    def handle_keys(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.rect.y -= self.speed  # Yukarı hareket
+        if keys[pygame.K_s]:
+            self.rect.y += self.speed  # Aşağı hareket
+        if keys[pygame.K_a]:
+            self.rect.x -= self.speed  # Sola hareket
+        if keys[pygame.K_d]:
+            self.rect.x += self.speed  # Sağa hareket
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
