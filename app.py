c2c1469e2ffb1b6f04e361507166b9a9721e624d
aylinkarabacak <aylin.karabacak38@gmail.com>
2025-04-29 16:40:12 +0300

    car

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import pygame
+from sources.Car import Car
+
 
 pygame.init()
 
@@ -6,15 +8,19 @@ screen = pygame.display.set_mode((1280,720))
 pygame.display.set_caption("Self Learning Car")
 
 clock = pygame.time.Clock()
+car = Car()
 
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			raise SystemExit
-	track = pygame.transform.scale(pygame.image.load("assets/track.png"), (1280, 720))
 	
+	track = pygame.transform.scale(pygame.image.load("assets/track.png"), (1280, 720))
+	car.handle_keys()
 	screen.blit(track, (0,0))
+	car.draw(screen)
+
 	
 	pygame.display.flip()
 	clock.tick(60)
--- sources/Car.py
+++ sources/Car.py
@@ -1,3 +1,22 @@
+import pygame
+
 class Car:
-	pass
-	
+    def __init__(self):
+        
+        self.rect = pygame.Rect(100, 100, 50, 30)  
+        self.color = (255, 0, 0)  
+        self.speed = 5  
+
+    def handle_keys(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.rect.y -= self.speed  
+        if keys[pygame.K_s]:
+            self.rect.y += self.speed  
+        if keys[pygame.K_a]:
+            self.rect.x -= self.speed  
+        if keys[pygame.K_d]:
+            self.rect.x += self.speed  
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
