c05f97c039d8ca1cab3fa81309357e5cccc85e9b
humeyra saritop <humeyrasaritop@gmail.com>
2025-05-04 11:46:23 +0300

    feat: kırmızı başlangıç noktası eklendi, yoksa program başlamıyor

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pygame
 from sources.Car import Car
+from sources.utils import find_start_position
 
 
 pygame.init()
@@ -8,6 +9,14 @@ screen = pygame.display.set_mode((1280,720))
 pygame.display.set_caption("Self Learning Car")
 
 clock = pygame.time.Clock()
+# ✅Arabayı oluşturmadan önce haritayı kontrol ediyoruz
+start_pos = find_start_position("assets/track2.png")
+if start_pos is None:
+    print("Haritada kirmizi nokta yok")  # ❌ Başlamadan çıkar
+    pygame.quit()
+    raise SystemExit
+
+
 car = Car()
 
 while True:
@@ -16,7 +25,7 @@ while True:
 			pygame.quit()
 			raise SystemExit
 	
-	track = pygame.transform.scale(pygame.image.load("assets/track.png"), (1280, 720))
+	track = pygame.transform.scale(pygame.image.load("assets/track2.png"), (1280, 720))
 	car.handle_keys()
 	screen.blit(track, (0,0))
 	car.draw(screen)
